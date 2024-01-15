230fb99b539a43a2173787130e23983c59b93459
Peterson-moura <peterson.moura@outlook.com.br>
2024-01-15 19:12:37 -0300

    styled_remove_.css

--- src/componentes/Header/index.js
+++ src/componentes/Header/index.js
@@ -1,14 +1,13 @@
-
 import Logo from "../Logo";
 import OpcoesHeader from "../OpcoesHeader";
 import IconesHeader from "../IconesHeader";
 import styled from "styled-components";
 
 const HeaderContainer = styled.header`
-background-color: #fff;
-  display: flex; 
+  background-color: #fff;
+  display: flex;
   justify-content: center;
-`
+`;
 
 // ...
 
