cae2eb64deb3a0d2a05f10c81ca9a49595b00229
Peterson-moura <peterson.moura@outlook.com.br>
2024-01-17 17:29:26 -0300

    _ultimosLancamentos_

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -25,25 +25,48 @@ const Subtitulo = styled.h3`
   margin-bottom: 40px;
 `;
 
+const Resultado = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+  cursor: pointer;
+  p {
+    width: 200px;
+  }
+  img {
+    width: 100px;
+  }
+  &:hover {
+    border: 1px solid white;
+  }
+`;
+
 function Pesquisa() {
-    const [ livrosPesquisados, setLivrosPesquisados ] = useState([])
+  const [livrosPesquisados, setLivrosPesquisados] = useState([]);
 
-    console.log(livrosPesquisados)
-
-    return (
-        <PesquisaContainer>
-            <Titulo>Já sabe por onde começar?</Titulo>
-            <Subtitulo>Encontre seu livro em nossa estante.</Subtitulo>
-            <Input
-                placeholder="Escreva sua próxima leitura"
-                onBlur={evento => {
-                    const textoDigitado = evento.target.value
-                    const resultadoPesquisa = livros.filter( livro => livro.nome.includes(textoDigitado) )
-                    setLivrosPesquisados(resultadoPesquisa)
-                }}
-            />
-        </PesquisaContainer>
-    )
+  return (
+    <PesquisaContainer>
+      <Titulo>Já sabe por onde começar?</Titulo>
+      <Subtitulo>Encontre seu livro em nossa estante.</Subtitulo>
+      <Input
+        placeholder="Escreva sua próxima leitura"
+        onBlur={(evento) => {
+          const textoDigitado = evento.target.value;
+          const resultadoPesquisa = livros.filter((livro) =>
+            livro.nome.includes(textoDigitado)
+          );
+          setLivrosPesquisados(resultadoPesquisa);
+        }}
+      />
+      {livrosPesquisados.map((livro) => (
+        <Resultado>
+          <p>{livro.nome}</p>
+          <img src={livro.src}></img>
+        </Resultado>
+      ))}
+    </PesquisaContainer>
+  );
 }
 
-export default Pesquisa
+export default Pesquisa;
