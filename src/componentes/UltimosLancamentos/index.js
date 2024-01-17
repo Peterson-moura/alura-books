2f13e87d18882995b450cfab0ee012e2672544b3
Peterson-moura <peterson.moura@outlook.com.br>
2024-01-17 18:30:47 -0300

    _utilizando_props

--- src/componentes/UltimosLancamentos/index.js
+++ src/componentes/UltimosLancamentos/index.js
@@ -1,43 +1,37 @@
 import { livros } from "./dadosUltimosLancamentos";
-import styled from 'styled-components'
+import { Titulo } from "../Titulo";
+import styled from "styled-components";
 
 const UltimosLancamentosContainer = styled.section`
-    background-color: #EBECEE;
-    padding-bottom: 20px;
-    display: flex;
-    flex-direction: column;
-`
-
-const Titulo = styled.h2`
-    width: 100%;
-    padding: 30px 0;
-    background-color: #FFF;
-    color: #EB9B00;
-    font-size: 36px;
-    text-align: center;
-    margin: 0;
-`
-
+  background-color: #ebecee;
+  padding-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+`;
 const NovosLivrosContainer = styled.div`
-    margin-top: 30px;
-    display: flex;
-    width: 100%;
-    justify-content: center;
-    cursor: pointer;
-`
-
+  margin-top: 30px;
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  cursor: pointer;
+`;
 
 function UltimosLancamentos() {
-    return (
-        <UltimosLancamentosContainer>
-        <Titulo>ULTIMOS LANCAMENTOS</Titulo>
-        <NovosLivrosContainer>
-            { livros.map( livro => (
-                <img src={livro.src}/>
-            ) ) }
-        </NovosLivrosContainer>
+  return (
+    <UltimosLancamentosContainer>
+      <Titulo 
+      cor="#EB9B00" 
+      tamanhoFonte='36px' 
+      >
+    ÚLTIMOS LANÇAMENTOS</Titulo>
+     
+      <NovosLivrosContainer>
+        {livros.map((livro) => (
+          <img src={livro.src} />
+        ))}
+      </NovosLivrosContainer>
     </UltimosLancamentosContainer>
-    )
+  );
 }
 
-export default UltimosLancamentos
+export default UltimosLancamentos;
